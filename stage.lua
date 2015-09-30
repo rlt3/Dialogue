2d7ff9243869d48d39e7db4b4b39cbef87be0cf1
rlt3 <leroy@bythehilt.com>
2015-09-30 14:51:42 -0400

    Just like lists are LISP's procedures and data, tables are similar for Lua. Looking forward to see how I will implement Dialogues and Actors.

--- stage.lua
+++ stage.lua
@@ -5,3 +5,15 @@ script = S{"draw", 400, 200}
 
 up = E{"update"};
 cons = E{"construct", 20, 40};
+
+-- An actor with two scripts.
+-- Actor{ {"draw", 400, 200}, {"weapon", "longsword"} };
+-- 
+-- A dialogue with two child Actors which have two scripts.
+-- Dialogue{
+--     { },
+--     { 
+--         { {"draw", 400, 200}, {"weapon", "longsword"} },
+--         { {"draw", 20, 40}, {"weapon", "knife"} }
+--     }
+-- };
