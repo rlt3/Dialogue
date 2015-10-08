85ca9161c768f4a609b71294bcd90aad3b6301c9
rlt3 <leroy@bythehilt.com>
2015-10-08 15:56:58 -0400

    Trying to understand how to do recursion with the Lua C api. It might actually be more trouble than its worth. I need to figure out how to do meta programming where writing legitimate Lua strings and calling it from C.

--- src/dialogue.c
+++ src/dialogue.c
@@ -2,6 +2,75 @@
 #include "script.h"
 #include "envelope.h"
 
+Dialogue *
+lua_check_dialogue (lua_State *L, int index)
+{
+    return lua_checkactor(L, index);
+}
+
+int
+dialogue_new (lua_State *L)
+{
+    Actor *actor, *child;
+    luaL_checktype(L, 1, LUA_TTABLE);  /* 1 */
+
+    /* push the Scripts part of the table to create an Actor */
+    lua_getglobal(L, "Dialogue");
+    lua_getfield(L, -1, "Actor");
+    lua_push_head(L, 1);
+    if (lua_call(L, 1, 1, 0))
+        luaL_error(L, "Failed to create Actor: %s", lua_tostring(L, -1));
+
+    actor = lua_check_actor(L, 2); /* 2 */
+
+    /* push the second part of the table -- the children */
+    lua_rawgeti(L, 1, 2);  /* 3 */
+
+    /* Create and add all the children */
+    lua_pushnil(L);
+    while (lua_next(L, 3)) { /* table @ 6, key @ 5 */
+
+        table_push_head(L, 6);  /* 7 */
+        lua_pushnil(L);
+        while (lua_next(L, 7)) { /* table @ 10, key @ 9 */
+
+            lua_method_push(L, actor, ACTOR_LIB, "child"); /* 11 & 12 */
+            lua_pushvalue(L, 10);
+            if (lua_call(L, 2, 1, 0))
+                luaL_error(L, "Failed to descend Dialogue children: %s", 
+                        lua_tostring(L, -1));
+
+            child = lua_check_actor(L, -1);
+
+            lua_pop(L, 2); /* pop table and return value */
+        }
+        lua_pop(L, 2); /* pop nil and table head */
+
+        lua_getglobal(L, "Dialogue");
+        lua_getfield(L, -1, "new");
+        lua_rawgeti(L, 6, 2);
+        if (lua_call(L, 2, 1, 0))
+            luaL_error(L, "Failed to descend Dialogue children: %s", 
+                    lua_tostring(L, -1));
+
+        lua_pop(L, 1);
+    }
+
+    lua_pop(L, 2); /* pop the nil key and table head */
+
+    /* 
+     * A dialogue is the head of a tree. All actors in a dialogue inherit 
+     * certain properties. These are those properties.
+     */
+    actor->mailbox = box;
+    actor->dialogue = actor;
+    actor->parent = actor;
+    lua_pushobject(L, ACTOR_LIB);
+
+    return 1;
+}
+
+
 int
 luaopen_Dialogue (lua_State *L)
 {
--- stage.lua
+++ stage.lua
@@ -6,11 +6,9 @@ tim_t = { {"draw", 2, 4} }
 
 hanks = Dialogue.Actor{ {"draw", 400, 200} }
 
--- A dialogue with two child Actors which have two scripts.
--- Dialogue{
---     { },
---     { 
---         { {"draw", 400, 200}, {"weapon", "longsword"} },
---         { {"draw", 20, 40}, {"weapon", "knife"} }
---     }
--- };
+--Dialogue{ {},
+--    {
+--        { { {"draw", 400, 200} }, {} }
+--        { { {"draw", 2, 4} }, {} }
+--    }
+--}
