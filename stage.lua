9a62df8487dd7b8ca8934a94e690d638d04b07f2
rlt3 <leroy@bythehilt.com>
2015-10-20 16:25:48 -0400

    Probe doesn't work (returns nil) when actor has greater than one script.

--- stage.lua
+++ stage.lua
@@ -2,12 +2,16 @@ Dialogue = require 'Dialogue'
 
 update = {"update"}
 
+--tim = Dialogue.Actor{ {"draw", 0, 0 }, {"weapon", "sword", "left"} }
 tim = Dialogue.Actor{ {"draw", 0, 0 } }
 
 tree = Dialogue.new{ 
     { {"draw", 0, 0 } }, 
     { 
-        { { {"draw", 50, 100 }, {"weapon", "axe", "down" } }, {} }, 
+        { { {"weapon", "axe", "down" } }, {} }, 
         { { {"draw", 2, 4 }, {"weapon", "longsword", "up"} }, {} } 
     }
 }
+
+x = tim:scripts()[1]
+y = tree:children()[1]:scripts()[1]
