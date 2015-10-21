92b843ca7fb7c5b14634a4cb61961bd51d11a30d
rlt3 <leroy@bythehilt.com>
2015-10-21 12:27:01 -0400

    Adding in some more tests to prove some facts

--- spec.lua
+++ spec.lua
@@ -1,16 +1,33 @@
 _G.Dialogue = require("Dialogue")
 
 describe("a Script", function()
-    local script = Dialogue.Actor.Script.new{ "weapon", "axe", "down" }
+    local script = script
+
+    it("needs at least one element in state table given", function()
+        local errfn = function()
+            script = Dialogue.Actor.Script.new{}
+        end
+        assert.has_error(errfn, "bad argument #1 to 'new' (Table needs to have a module name!)")
+    end)
 
     it("fails sending if not loaded", function()
         local errfn = function()
+            script = Dialogue.Actor.Script.new{ "weapon", "axe", "down" }
             script:send{ "attack" }
         end
         assert.has_error(errfn, "Script isn't loaded!")
     end)
 
+    it("needs a valid lua object table with a new function to be loaded", function()
+        local errfn = function()
+            script = Dialogue.Actor.Script.new{ "collision" }
+            script:load()
+        end
+        assert.has_error(errfn, "Require failed for module collision")
+    end)
+
     it("holds private, internal state responding only to and by messages", function()
+        script = Dialogue.Actor.Script.new{ "weapon", "axe", "down" }
         script:load()
         script:send{ "attack" }
         assert.is_equal(script:probe("durability"), 9)
