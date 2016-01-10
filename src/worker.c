649af6224edb49209d1620bb3fc5d614c8c8aff1
rlt3 <leroy@bythehilt.com>
2016-01-10 16:14:38 -0500

    Setting up the Worker thread. Need to push the Dialogue table & functions to each Worker state next.

--- src/worker.c
+++ src/worker.c
@@ -17,108 +17,68 @@ struct Worker {
 /*
  * Actually do the actions of our Dialogue.
  */
-//void *
-//worker_thread (void *arg)
-//{
-//    const char *error;
-//    const int dialogue_table = 1;
-//    int i, len, args, top;
-//    Worker *worker = arg;
-//    lua_State *W = worker->L;
-//
-//    pthread_mutex_lock(&worker->mutex);
-//
-//    /* push the initial two tables onto the stack */
-//    lua_getglobal(W, "Dialogue");
-//
-//    while (1) {
-//        if (!worker->working)
-//            break;
-//        
-//        /*
-//         * TODO:
-//         *    I'm calling lua_checkstack inside mailbox_pop_all, but not 
-//         * anywhere else afterwards. So, if mailbox_pop_all pushes N actions,
-//         * but it can't handle N+1, then I get some bad errors.
-//         *
-//         *    I need both the Mailbox and Worker stacks to work in unison. 
-//         * mailbox_pop_all should be able to push less than all actions if 
-//         * needed, but this stack deal is *most likely* the cause of these
-//         * issues -- not 100% certain though
-//         */
-//
-//        /* wait if there's no work */
-//        if (mailbox_pop_all(W, worker->mailbox) == 0) {
-//            //printf("%p waiting %p\n", worker, worker->mailbox);
-//            pthread_cond_wait(&worker->wait_cond, &worker->mutex);
-//            //printf("%p signaled\n", worker);
-//            continue;
-//        }
-//        
-//        //printf("%p working\n", worker);
-//        for (top = lua_gettop(W); top > dialogue_table; top--) {
-//            len  = luaL_len(W, top);
-//            args = len - 1;
-//
-//            /* push the action type to see if it exists */
-//            lua_rawgeti(W, top, 1);
-//            lua_gettable(W, dialogue_table);
-//
-//            if (!lua_isfunction(W, -1)) {
-//                lua_rawgeti(W, top, 1);
-//                error = lua_tostring(W, -1);
-//                printf("`%s' is not an Action recognized by Dialogue!\n", error);
-//                lua_pop(W, 2); /* action type and the `function' */
-//                goto next;
-//            }
-//
-//            /* push the rest of the table as arguments */
-//            for (i = 2; i <= len; i++)
-//                lua_rawgeti(W, top, i);
-//
-//            /*
-//             * Returns a table of actions to resend and a boolean to determine if
-//             * the messages should be resent through the Director or if this Worker
-//             * should just redo it
-//             *
-//             * 0 - Normal case, can pus nil for table
-//             * 1 - Resend the current message
-//             * 2 - Resend the list of messages
-//             * 3 - Redo - push the list onto the Worker stack and loop
-//             */
-//            if (lua_pcall(W, args, 0, 0)) {
-//                error = lua_tostring(W, -1);
-//                printf("%s\n", error);
-//                lua_pop(W, 1); /* error string */
-//            }
-//next:
-//            worker->processed++;
-//            lua_pop(W, 1); /* the top action */
-//        }
-//    }
-//
-//    if (top > 1)
-//        printf("%p quit with %d left\n", worker, top);
-//
-//    pthread_mutex_unlock(&worker->mutex);
-//
-//    return NULL;
-//}
+void *
+worker_thread (void *arg)
+{
+    const char *error;
+    const int dialogue_table = 1;
+    int i, len, args, top;
+    Worker *worker = arg;
+    lua_State *W = worker->L;
+
+    /* push the initial two tables onto the stack */
+    //lua_getglobal(W, "Dialogue");
+
+get_work:
+    while (worker->working) {
+        if (mailbox_pop_all(W, worker->mailbox) == 0)
+            goto get_work;
+        
+        for (top = lua_gettop(W); top > dialogue_table; top--) {
+        //    len  = luaL_len(W, top);
+        //    args = len - 1;
+
+        //    /* push the action type to see if it exists */
+        //    lua_rawgeti(W, top, 1);
+        //    lua_gettable(W, dialogue_table);
+
+        //    if (!lua_isfunction(W, -1)) {
+        //        lua_rawgeti(W, top, 1);
+        //        error = lua_tostring(W, -1);
+        //        printf("`%s' is not an Action recognized by Dialogue!\n", error);
+        //        lua_pop(W, 2); /* action type and the `function' */
+        //        goto next;
+        //    }
+
+        //    /* push the rest of the table as arguments */
+        //    for (i = 2; i <= len; i++)
+        //        lua_rawgeti(W, top, i);
+
+        //    if (lua_pcall(W, args, 0, 0)) {
+        //        error = lua_tostring(W, -1);
+        //        printf("%s\n", error);
+        //        lua_pop(W, 1); /* error string */
+        //    }
+next:
+            worker->processed++;
+            lua_pop(W, 1); /* the top action */
+        }
+    }
+
+    return NULL;
+}
 
 Worker *
 worker_start (lua_State *L)
 {
     Worker *worker = malloc(sizeof(*worker));
     /* TODO: check memory here */
-    //worker->L = lua_newthread(L);
-    ///* ref (which pops) the thread object so we control garbage collection */
-    //worker->ref = luaL_ref(L, LUA_REGISTRYINDEX);
+    worker->L = luaL_newstate();
+    /* TODO: check memory here */
     worker->mailbox = mailbox_create();
-    //worker->working = 1;
-    //worker->processed = 0;
-    //pthread_mutex_init(&worker->mutex, NULL);
-    //pthread_cond_init(&worker->wait_cond, NULL);
-    //pthread_create(&worker->thread, NULL, worker_thread, worker);
+    worker->working = 1;
+    worker->processed = 0;
+    pthread_create(&worker->thread, NULL, worker_thread, worker);
     return worker;
 }
 
