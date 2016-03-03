3f47624b9a5884936a5b98a548a3b2b9a04d9c2f
rlt3 <leroy@bythehilt.com>
2016-03-02 23:31:39 -0500

    There's some issues implementing the wait condition correctly. This probably requires me to combine both the Worker and Mailbox into a single entity since they are so tightly coupled.

--- src/worker.c
+++ src/worker.c
@@ -10,9 +10,10 @@ struct Worker {
     lua_State *L;
     pthread_t thread;
     pthread_mutex_t mutex;
+    pthread_cond_t cond;
     Mailbox *mailbox;
+    enum { WORKING, WAITING, DONE } state;
     int processed;
-    int working;
 };
 
 void
@@ -22,6 +23,8 @@ worker_process_action (Worker *worker, const int action_table)
     //int len  = luaL_len(W, action_table);
     //int args = len - 1;
     //int i;
+    
+    printf("%p got action @ %d!\n", (void*) worker, action_table);
 
     lua_rawgeti(W, action_table, 1);
 
@@ -49,9 +52,13 @@ worker_process_action (Worker *worker, const int action_table)
     case ACTION_DELETE:
         puts("Delete");
         break;
+
+    default:
+        puts("No Action!!!");
+        break;
     }
 
-    lua_pop(W, 1);
+    lua_pop(W, 2); /* first element of table & table itself */
 }
 
 void *
@@ -59,93 +66,50 @@ worker_thread (void *arg)
 {
     Worker *worker = arg;
     lua_State *W = worker->L;
-    int top, working;
+    int top;
 
+    /*
+    enum { WORKING, WAITING, DONE } state;
     while (1) {
         pthread_mutex_lock(&worker->mutex);
-        working = worker->working;
+        state = worker->state;
         pthread_mutex_unlock(&worker->mutex);
 
-        if (!working)
+        if (state == DONE)
             break;
 
-	    /* 
-         * TODO: should be a cond_wait on if the mailbox is empty or not.
-         */
         if (mailbox_pop_all(W, worker->mailbox) == 0) {
             usleep(1000);
             continue;
         }
 
-        for (top = lua_gettop(W); top >= 1; top--) {
-            printf("%p got action!\n", (void*) worker);
-            //worker_process_action(worker, top);
-        }
+        for (top = lua_gettop(W); top > 0; top--)
+            worker_process_action(worker, top);
     }
+    */
+
+    pthread_mutex_lock(&worker->mutex);
+
+    while (worker->state != DONE) {
+        /* 
+         * TODO: Need to be able to wake from Mailbox when worker "goes to
+         * sleep" with a minimal number of locks.
+         *
+         * Perhaps N retrys before acquiring the mailbox's lock, setting
+         * a flag which tells the mailbox to signal the worker on the
+         * next action it receives.
+         */
+        if (mailbox_pop_all(W, worker->mailbox) == 0)
+            pthread_cond_wait(&worker->cond, &worker->mutex);
+
+        for (top = lua_gettop(W); top > 0; top--)
+            worker_process_action(worker, top);
+    }
+
+    pthread_mutex_unlock(&worker->mutex);
 
     return NULL;
 }
-
-///*
-// * Actually do the actions of our Dialogue.
-// */
-//void *
-//worker_thread (void *arg)
-//{
-//    const char *error;
-//    const int director_table = 1;
-//    int i, len, args, top;
-//    Worker *worker = arg;
-//    lua_State *W = worker->L;
-//
-//    lua_getglobal(W, "Director");
-//
-//get_work:
-//    while (worker->working) {
-//        if (mailbox_pop_all(W, worker->mailbox) == 0)
-//            goto get_work;
-//        
-//        for (top = lua_gettop(W); top > director_table; top--) {
-//            len  = luaL_len(W, top);
-//            args = len - 1;
-//
-//            /* push the action type to see if it exists */
-//            lua_rawgeti(W, top, 1);
-//            lua_gettable(W, director_table);
-//
-//            if (!lua_isfunction(W, -1)) {
-//                lua_rawgeti(W, top, 1);
-//                error = lua_tostring(W, -1);
-//                printf("`%s' is not an Action recognized by Dialogue!\n", error);
-//                lua_pop(W, 2); /* action type and the `function' */
-//                goto next;
-//            }
-//
-//            /* push the director's 'self' reference for the method call */
-//            lua_pushvalue(W, director_table);
-//
-//            /* push the rest of the table as arguments */
-//            for (i = 2; i <= len; i++)
-//                lua_rawgeti(W, top, i);
-//
-//            /* push the Worker's pointer at the end as an optional argument */
-//            lua_pushlightuserdata(W, worker);
-//
-//            if (lua_pcall(W, args + 2, 0, 0)) {
-//                error = lua_tostring(W, -1);
-//                printf("%s\n", error);
-//                lua_pop(W, 1); /* error string */
-//            }
-//
-//            printf("top: %d\n", lua_gettop(W));
-//next:
-//            worker->processed++;
-//            lua_pop(W, 1); /* the top action */
-//        }
-//    }
-//
-//    return NULL;
-//}
 
 /*
  * Create a worker without a thread.
@@ -176,10 +140,12 @@ worker_create ()
         goto exit;
     }
 
-    worker->working = 1;
+    worker->state = WORKING;
     worker->processed = 0;
+
     luaL_openlibs(worker->L);
     pthread_mutex_init(&worker->mutex, NULL);
+    pthread_cond_init(&worker->cond, NULL);
 
 exit:
     return worker;
@@ -210,7 +176,14 @@ exit:
 int
 worker_pop_action (Worker *worker, lua_State *L)
 {
-    return mailbox_push_top(L, worker->mailbox);
+    int ret = mailbox_push_top(L, worker->mailbox);
+
+    /*
+    if (ret)
+        pthread_cond_signal(&worker->cond);
+    */
+
+    return ret;
 }
 
 /*
@@ -220,7 +193,8 @@ void
 worker_stop (Worker *worker)
 {
     pthread_mutex_lock(&worker->mutex);
-    worker->working = 0;
+    worker->state = DONE;
+    //pthread_cond_signal(&worker->cond);
     pthread_mutex_unlock(&worker->mutex);
 
     pthread_join(worker->thread, NULL);
