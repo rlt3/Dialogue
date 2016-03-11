5c19439081e5ecfe546a3421b8624396c9bff6b1
rlt3 <leroy@bythehilt.com>
2016-03-11 08:49:21 -0500

    Adding a todo for the options for Dialogue.

--- src/dialogue.c
+++ src/dialogue.c
@@ -36,6 +36,24 @@ static const luaL_Reg dialogue_metamethods[] = {
     { NULL, NULL }
 };
 
+/*
+ * TODO:
+ *  Args can be passed to require like this, where ... can be a table.
+ *      `Dialogue = require 'Dialogue' (...)`
+ *
+ *  Change the above option system to look for that table which uses a name 
+ *  that is statically defined above.
+ *
+ *       `{ WORKER_IS_MAIN , "main_thread_is_worker", 0 }`
+ *       `{   WORKER_COUNT , "worker_count", 4 }`
+ *
+ *  Then the array is updated as the iterator makes its way through each row.
+ *  It looks the table for the key (mid column), if it exists, updates index 
+ *  (first column). third column is default.
+ *
+ *  This lets the program's options be set through the command-line switches,
+ *  the script, and through require if compiled the shared object.
+ */
 int
 luaopen_Dialogue (lua_State *L)
 {
